d67c98478e8a530f35a97e2e476ad3f6475ffd03
taufik <taufik@codelabs.co.id>
2026-03-12 10:32:26 +0700

    fix: bgcolor on email yahoo

--- src/components/emails/PaymentConfirmation.tsx
+++ src/components/emails/PaymentConfirmation.tsx
@@ -42,7 +42,7 @@ export const PaymentConfirmationEmail = ({
           maxWidth: '600px',
           margin: '0 auto',
           padding: '20px',
-          backgroundColor: '#181b15a',
+          backgroundColor: '#0f1f0f',
           borderRadius: '12px',
           border: '1px solid #ffff'
         }}>
